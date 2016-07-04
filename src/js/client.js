90dba744dc4af024bd49487b3becac2558f6fe7e
Kyle Richardson <kylerichardson@Kyles-MacBook-Pro-2.local>
2016-07-04 13:29:19 -0400

    Create and export Layout/Note components

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component{
-  render(){
-    return(
-      <h1>App rendering</h1>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 var app = document.getElementById('app');
 
