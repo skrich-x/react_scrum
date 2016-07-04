7a1d85f41c63ef6694f6ec60f7404d961b6f82bd
Kyle Richardson <kylerichardson@Kyles-MacBook-Pro-2.local>
2016-07-04 17:24:26 -0400

    Add component note.js class & styles

--- src/js/components/Note.js
+++ src/js/components/Note.js
@@ -3,7 +3,9 @@ import React from "react";
 export default class Note extends React.Component{
   render(){
     return(
-      <h1>App rendering</h1>
+      <div className="note">
+        <h1>App rendering!</h1>        
+      </div>
     );
   }
 }
